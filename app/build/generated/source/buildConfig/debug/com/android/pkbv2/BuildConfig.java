271490eab4721fc7c1065c6fb476588aa9e5dfc2
Twin Edo <venganzdektor26@gmail.com>
2019-05-08 09:59:52 -0700

    rename pkg

--- app/build/generated/source/buildConfig/debug/com/android/pkbv2/BuildConfig.java
+++ app/build/generated/source/buildConfig/debug/com/android/pkbv2/BuildConfig.java
@@ -5,7 +5,7 @@ package com.android.pkbv2;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "com.android.pkbv2";
+  public static final String APPLICATION_ID = "com.pkbv2";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 2;
